b3a8174130b3b61889b302c59ae3a7f9c303a763
amirkairzhan <amirkairzhan@bk.ru>
2024-12-21 04:00:26 +0500

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-    },
-  ],
-  {
-    future: {
-      v7_relativeSplatPath: true,
-    },
-  }
-);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
